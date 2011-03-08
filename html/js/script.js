98ebb29e1e5172bac875108a4ccd8ca6f952ca01
Jannis Hermanns <jannis@gmail.com>
2011-03-08 17:37:32 +0100

    Mocked fragment list, display fragments

--- html/js/script.js
+++ html/js/script.js
@@ -2,9 +2,49 @@
 
 */
 
+(function( $ ){
+
+  var Bricker = {
+    fetchFragments: function(prefix) {
+      if (prefix != '/') return []; 
+      return ['container', 'content', 'transition'];
+    }, 
+
+    buildFragmentLi: function (prefix, fragment) {
+      var li = $('<li>'+fragment+'</li>'); 
+      li.data('prefix', [prefix, fragment].join('/'));
+      return li;
+    }
+  };
+
+  $.fn.bricker = function( prefix ) {  
+
+    // Prepare shit
+    var fragments = Bricker.fetchFragments(prefix),
+        container = $(this),
+               ul = $('<ul class="fragments" />');
+
+    // Fill fragment list
+    $.each(fragments,function(i,f){
+      var li = Bricker.buildFragmentLi(prefix, f);
+      ul.append(li); 
+    });
+
+    // Add fragment list
+    container.append(ul); 
+
+    // Add bricker container
+    container.append($('<div />'));
+  }
+
+})(jQuery);
 
 
+$(document).ready(function(){
 
+  $('#main').bricker('/');
+
+});
 
 
 
