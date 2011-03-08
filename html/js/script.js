99e2ad74cafbf419507101da289095c3dc31f33e
Jannis Hermanns <jannis@gmail.com>
2011-03-08 23:56:14 +0100

    Data from JSON

--- html/js/script.js
+++ html/js/script.js
@@ -6,16 +6,29 @@
 
   var Bricker = {
     fetchFragments: function(prefix, callback) {
+      var url = '../api/'+(prefix||'index')+'.json' 
+      console.log(url);
+      $.ajax({ url: url
+             , dataType: 'json'
+             , data: {}
+             , success: function(data, textStatus, jqXHR){
+                callback(data); 
+               }
+             , error: function(jqXHR, textStatus, errorThrown) {
+               console.log("ERRORS");
+               console.log(errorThrown);
+             }
+      });
 
-      if (prefix == '') return callback(['container', 'content/_id_', 'transition']);
-      if (prefix == '/content/_id_') return callback(['eins', 'zwo']);
-      if (prefix == '/content/_id_/zwo') return callback(['foo']);
-      return { foo: 1}; 
+      // if (prefix == '') return callback(['container', 'content/_id_', 'transition']);
+      // if (prefix == '/content/_id_') return callback(['eins', 'zwo']);
+      // if (prefix == '/content/_id_/zwo') return callback(['foo']);
+      // callback( { foo: 1} ); 
     }, 
 
     buildFragmentLi: function (prefix, fragment) {
-      var li = $('<li>'+fragment+'</li>'); 
-      li.data('prefix', [prefix, fragment].join('/'));
+      var li = $('<li>'+fragment.prefix+'</li>'); 
+      li.data('prefix', [prefix, fragment.prefix].join('/'));
       return li;
     },
 
@@ -28,21 +41,21 @@
       return ul;
     },
 
-    displayFragments: function(container, prefix, fragments) {
+    displayFragments: function(container, prefix, data) {
 
       // Empty out
       container.children().remove();
 
       // Build another list or display documentation
-      if (fragments instanceof Array) {
+      if (data.fragments instanceof Array) {
 
-        var ul = Bricker.buildFragmentList(prefix, fragments);
+        var ul = Bricker.buildFragmentList(prefix, data.fragments);
         container.append(ul); 
         container.append($('<div class="bricker"></div>'));
 
       // Display this endpoint
       } else {
-        container.append($('<div>'+fragments+'</div>'));
+        container.append($('<div>endpoint</div>'));
       }
     }
 
