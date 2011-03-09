788b3f303ce2ee7a9e94f5184dd8eafca555cf49
Jannis Hermanns <jannis@gmail.com>
2011-03-09 15:58:07 +0100

    Fetching endpoints from .txt

--- html/js/script.js
+++ html/js/script.js
@@ -3,19 +3,27 @@ var Bricker = {
 
   fetchFragments: function(prefix, callback) {
     prefix  = prefix == '' ? '/index' : prefix;
-    var url = '../api'+prefix+'.json' 
-    console.log(url);
+    var url = '../api'+prefix+'.json?'+Math.random(); 
     $.ajax({ url: url
            , data: {}
            , success: function(data, textStatus, jqXHR){
               callback(data); 
              }
-           , error: function(jqXHR, textStatus, errorThrown) {
-             callback({'endpoint': [textStatus, url].join(' ')});
-           }
+           , error: function(jqXHR, textStatus, errorThrown) {}
     });
   }, 
 
+  fetchEndpoints: function(prefix, callback) {
+    var url = '../api'+prefix+'/endpoints.txt'; 
+    $.ajax({ url: url
+           , data: {}
+           , success: function(data, textStatus, jqXHR){
+              callback(data); 
+             }
+           , error: function(jqXHR, textStatus, errorThrown) {}
+    });
+  }, 
+  
   buildFragmentLi: function (prefix, fragment) {
     var li = $('<li>'+fragment.prefix+'</li>'); 
     li.data('prefix', [prefix, fragment.prefix].join(''));
@@ -34,16 +42,14 @@ var Bricker = {
   buildEndpoint: function(prefix, data) {
    var container = $('<div class="endpoint"/>');
    container.append($('<h1>'+prefix+'</h1>'));
-   container.append ($('<pre>'+data.endpoint+'</pre>')); 
-  
+   container.append ($('<pre>'+data+'</pre>')); 
+
    return container;
   },
 
   displayFragments: function(container, prefix, data) {
-
     // Empty out
     container.children().remove();
-    container.parent().children('.endpoint').remove();
 
     // Build another list or display documentation
     if(data.fragments instanceof Array) {
@@ -51,11 +57,12 @@ var Bricker = {
       container.append(ul); 
       container.append($('<div class="bricker"></div>'));
     }
+  },
 
-    // Display this endpoint
-    if (data.endpoint) {
-      container.append(Bricker.buildEndpoint(prefix, data));
-    }
+  displayEndpoints: function(container, prefix, endpoints) {
+   var endpoint = Bricker.buildEndpoint(prefix, endpoints);
+   container.parent().children('.endpoint').remove();
+   container.append(endpoint);
   },
 
   handleFragmentClick: function() {
@@ -73,7 +80,10 @@ var Bricker = {
     var container = $(this);
     Bricker.fetchFragments(prefix, function(f) {
       Bricker.displayFragments(container, prefix, f); 
-    } );
+      Bricker.fetchEndpoints(prefix, function(e) {
+        Bricker.displayEndpoints(container, prefix, e);
+      });
+    });
   }
 
 })(jQuery);
