990e41d718f33ccaaa1c01ca6d170f974999df67
Jannis Hermanns <jannis@gmail.com>
2011-04-11 22:56:28 +0200

    Refactor for fun and glory. Oh wait, is it 11pm?????

--- public/js/mylibs/parser.js
+++ public/js/mylibs/parser.js
@@ -53,20 +53,32 @@ var exParser = {
     if(match = this.lines[this.i].match(/^##?[ ]*(.*)/)) {
       this.data.description += match[1]+"\n";
     } else if (match = this.lines[this.i].match(/^([A-Z]{3,6})([:]?[ ]+)(.*)$/)) {
-      this.data.method = match[1]; 
-      this.data.uri    = match[3];
-      this.current = "request";
+      this.parseMethod(match);
     } else if (match = this.lines[this.i].match(/^Response:[ ]?(.*)$/)) {
-      this.current = "response";
-      this.data['response_code'] = match[1];
-      this.i++;
+      this.switchMode(match);
     } else if (this.current) {
-      var stop = this.current == 'request' ? 'Response' : null;
-      var vars = varParser.parse(this.lines, this.i, stop); 
-      this.i = vars[0]++;
-      this.data[this.current] = vars[1];
+      this.parseVars();
     }
     return this.advance();
+  },
+
+  parseMethod: function(match){
+    this.data.method = match[1]; 
+    this.data.uri    = match[3];
+    this.current = "request";
+  },
+
+  switchMode: function(match){
+    this.current = "response";
+    this.data['response_code'] = match[1];
+    this.i++;
+  },
+
+  parseVars: function() {
+    var stop = this.current == 'request' ? 'Response' : null;
+    var vars = varParser.parse(this.lines, this.i, stop); 
+    this.i = vars[0]++;
+    this.data[this.current] = vars[1];
   },
 
   advance: function() {
